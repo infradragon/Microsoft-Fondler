009d0ba2b77604c4e533f3addc0a8c7e1a32c433
infradragon <72665124+infradragon@users.noreply.github.com>
2023-06-20 13:41:05 -0500

    Add files via upload
    literally does nothing rn, just a decapitated mas script

---
+++ FONDLER.cmd
@@ -0,0 +1,186 @@
+@setlocal DisableDelayedExpansion
+@echo off
+
+:: Most of the beginning of this file was stolen from MAS (https://massgrave.dev/)
+:: Thank you to awuctl and lyssa69 for the ideas
+::
+::===============================================================================
+
+:: Re-launch the script with x64 process if it was initiated by x86 process on x64 bit Windows
+:: or with ARM64 process if it was initiated by x86/ARM32 process on ARM64 Windows
+
+set "_cmdf=%~f0"
+for %%# in (%*) do (
+if /i "%%#"=="r1" set r1=1
+if /i "%%#"=="r2" set r2=1
+)
+
+if exist %SystemRoot%\Sysnative\cmd.exe if not defined r1 (
+setlocal EnableDelayedExpansion
+start %SystemRoot%\Sysnative\cmd.exe /c ""!_cmdf!" %* r1"
+exit /b
+)
+
+:: Re-launch the script with ARM32 process if it was initiated by x64 process on ARM64 Windows
+
+if exist %SystemRoot%\SysArm32\cmd.exe if %PROCESSOR_ARCHITECTURE%==AMD64 if not defined r2 (
+setlocal EnableDelayedExpansion
+start %SystemRoot%\SysArm32\cmd.exe /c ""!_cmdf!" %* r2"
+exit /b
+)
+
+::  Set Path variable, it helps if it is misconfigured in the system
+
+set "PATH=%SystemRoot%\System32;%SystemRoot%\System32\wbem;%SystemRoot%\System32\WindowsPowerShell\v1.0\"
+if exist "%SystemRoot%\Sysnative\reg.exe" (
+set "PATH=%SystemRoot%\Sysnative;%SystemRoot%\Sysnative\wbem;%SystemRoot%\Sysnative\WindowsPowerShell\v1.0\;%PATH%"
+)
+
+::  Check LF line ending
+
+pushd "%~dp0"
+>nul findstr /rxc:".*" "%~nx0"
+if not %errorlevel%==0 (
+echo:
+echo Error: Script either has LF line ending issue, or it failed to read itself.
+echo:
+popd
+ping 127.0.0.1 -n 6 > nul
+exit /b
+)
+popd
+
+::========================================================================================================================================
+
+cls
+color 07
+title  Microsoft-Fondler
+
+set _args=
+set _elev=
+set _MASunattended=
+
+set _args=%*
+if defined _args set _args=%_args:"=%
+if defined _args (
+for %%A in (%_args%) do (
+if /i "%%A"=="-el"                    set _elev=1
+)
+)
+
+if defined _args echo "%_args%" | find /i "/" >nul && set _MASunattended=1
+
+::========================================================================================================================================
+
+set winbuild=1
+set "nul=>nul 2>&1"
+set psc=powershell.exe
+for /f "tokens=6 delims=[]. " %%G in ('ver') do set winbuild=%%G
+
+set _NCS=1
+if %winbuild% LSS 10586 set _NCS=0
+if %winbuild% GEQ 10586 reg query "HKCU\Console" /v ForceV2 2>nul | find /i "0x0" 1>nul && (set _NCS=0)
+
+call :_colorprep
+
+set "nceline=echo: &echo ==== ERROR ==== &echo:"
+set "eline=echo: &call :_color %Red% "==== ERROR ====" &echo:"
+
+::========================================================================================================================================
+
+if %winbuild% LSS 7600 (
+%nceline%
+echo Unsupported OS version detected.
+echo Project is supported only for Windows 7/8/8.1/10/11 and their Server equivalent.
+goto MASend
+)
+
+for %%# in (powershell.exe) do @if "%%~$PATH:#"=="" (
+%nceline%
+echo Unable to find powershell.exe in the system.
+echo Aborting...
+goto MASend
+)
+
+::========================================================================================================================================
+
+::  Fix for the special characters limitation in path name
+
+set "_work=%~dp0"
+if "%_work:~-1%"=="\" set "_work=%_work:~0,-1%"
+
+set "_batf=%~f0"
+set "_batp=%_batf:'=''%"
+
+set _PSarg="""%~f0""" -el %_args%
+
+set "_ttemp=%temp%"
+
+setlocal EnableDelayedExpansion
+
+::========================================================================================================================================
+
+echo "!_batf!" | find /i "!_ttemp!" 1>nul && (
+if /i not "!_work!"=="!_ttemp!" (
+%nceline%
+echo Script is launched from the temp folder,
+echo Most likely you are running the script directly from an archive file.
+echo:
+echo Extract the archive file and launch the script from the extracted folder.
+goto MASend
+)
+)
+
+::========================================================================================================================================
+
+::  Elevate script as admin and pass arguments and preventing loop
+
+>nul fltmc || (
+if not defined _elev %nul% %psc% "start cmd.exe -arg '/c \"!_PSarg:'=''!\"' -verb runas" && exit /b
+%nceline%
+echo This script require administrator privileges.
+echo To do so, right click on this script and select 'Run as administrator'.
+goto MASend
+)
+
+if not exist "%SystemRoot%\Temp\" mkdir "%SystemRoot%\Temp" 1>nul 2>nul
+
+::========================================================================================================================================
+
+::  Run script with parameters in unattended mode
+
+set _elev=
+if defined _args echo "%_args%" | find /i "/S" %nul% && (set "_silent=%nul%") || (set _silent=)
+if defined _args echo "%_args%" | find /i "/" %nul% && (
+echo "%_args%" | find /i "/HWID"   %nul% && (setlocal & (call :HWIDActivation   %_args% %_silent%) & cls & endlocal)
+echo "%_args%" | find /i "/KMS38"  %nul% && (setlocal & (call :KMS38Activation  %_args% %_silent%) & cls & endlocal)
+echo "%_args%" | find /i "/KMS-"   %nul% && (setlocal & (call :KMSActivation    %_args% %_silent%) & cls & endlocal)
+echo "%_args%" | find /i "/Insert" %nul% && (setlocal & (call :insert_hwidkey   %_args% %_silent%) & cls & endlocal)
+exit /b
+)
+
+::========================================================================================================================================
+
+setlocal DisableDelayedExpansion
+
+::  Check desktop location
+
+set _desktop_=
+for /f "skip=2 tokens=2*" %%a in ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v Desktop') do call set "_desktop_=%%b"
+if not defined _desktop_ for /f "delims=" %%a in ('%psc% "& {write-host $([Environment]::GetFolderPath('Desktop'))}"') do call set "_desktop_=%%a"
+
+set "_pdesk=%_desktop_:'=''%"
+setlocal EnableDelayedExpansion
+
+::========================================================================================================================================
+
+:+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+:MASend
+echo:
+if defined _MASunattended timeout /t 2 & exit /b
+echo Press any key to exit...
+pause >nul
+exit /b
+
+::End::
