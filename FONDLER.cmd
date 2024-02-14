d1f9076a2122491227b0cc10964d7919a7a9eebb
infradragon <72665124+infradragon@users.noreply.github.com>
2024-02-14 10:01:14 -0600

    Update FONDLER.cmd

--- FONDLER.cmd
+++ FONDLER.cmd
@@ -280,38 +280,6 @@ reg add "HKLM\SOFTWARE\Microsoft\Windows\Windows Error Reporting" /v Disabled /t
 reg add "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\LocationAndSensors" /v DisableLocation /t REG_DWORD /d 1 /f
 reg add "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\AppPrivacy" /v LetAppsAccessLocation /t REG_DWORD /d 2 /f
 
-
-:: Remove Edge and default UWP apps
-powershell -Command "PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {
-
-$regView = [Microsoft.Win32.RegistryView]::Registry32
-$microsoft = [Microsoft.Win32.RegistryKey]::OpenBaseKey([Microsoft.Win32.RegistryHive]::LocalMachine, $regView).
-OpenSubKey('SOFTWARE\Microsoft', $true)
-
-$edgeClient = $microsoft.OpenSubKey('EdgeUpdate\ClientState\{56EB18F8-B008-4CBD-B6D2-8C97FE7E9062}', $true)
-if ($null -ne $edgeClient.GetValue('experiment_control_labels')) {
-	$edgeClient.DeleteValue('experiment_control_labels')
-}
-
-$microsoft.CreateSubKey('EdgeUpdateDev').SetValue('AllowUninstall', '')
-
-$uninstallRegKey = $microsoft.OpenSubKey('Windows\CurrentVersion\Uninstall\Microsoft Edge')
-$uninstallString = $uninstallRegKey.GetValue('UninstallString') + ' --force-uninstall'
-Start-Process cmd.exe "/c $uninstallString" -WindowStyle Hidden
-
-$appxStore = '\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore'
-$pattern = "HKLM:$appxStore\InboxApplications\Microsoft.MicrosoftEdge_*_neutral__8wekyb3d8bbwe"
-$key = (Get-Item -Path $pattern).PSChildName
-reg delete "HKLM$appxStore\InboxApplications\$key" /f
-
-$SID = (New-Object System.Security.Principal.NTAccount($env:USERNAME)).Translate([Security.Principal.SecurityIdentifier]).Value
-
-New-Item -Path "HKLM:$appxStore\EndOfLife\$SID\Microsoft.MicrosoftEdge_8wekyb3d8bbwe" -Force
-
-Get-AppxPackage -Name Microsoft.MicrosoftEdge | Remove-AppxPackage
-
-Remove-Item -Path "HKLM:$appxStore\EndOfLife\$SID\Microsoft.MicrosoftEdge_8wekyb3d8bbwe"
-
 Disable-WindowsOptionalFeature -FeatureName Internet-Explorer-Optional-amd64 –Online
 Get-AppxProvisionedPackage -Online | Where-Object {$_.PackageName -Like "Microsoft.BingNews"} | ForEach-Object { Remove-AppxProvisionedPackage -Online -PackageName $_.PackageName}
 Get-AppxPackage Microsoft.BingNews | Remove-AppxPackage
