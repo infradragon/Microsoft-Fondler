4beb5a13ac31d3340d0161430644b2de7fe8edcc
infradragon <infradragon7@gmail.com>
2025-02-25 16:42:08 -0600

    new onedrive script

--- FONDLER.cmd
+++ FONDLER.cmd
@@ -482,6 +482,9 @@ reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WDI\{9c5a40da-b965-4fc3-8781-8
 :: Disable Program Compatibility Assistant telemetry
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\AppCompat" /v "AllowTelemetry" /t REG_DWORD /d 0 /f
 
+:: Disable Smart App Control https://github.com/Atlas-OS/Atlas/blob/main/src/playbook/Configuration/atlas/components.yml#L8
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\CI\Policy" /v "VerifiedAndReputablePolicyState" /t REG_DWORD /d 0 /f
+
 :: Turn off location servies and location history
 reg add "HKLM\Software\Policies\Microsoft\Windows\LocationAndSensors" /v "DisableLocation" /t REG_DWORD /d 1 /f
 reg add "HKLM\Software\Policies\Microsoft\Windows\AppPrivacy" /v "LetAppsAccessLocation" /t REG_DWORD /d 2 /f
@@ -787,11 +790,7 @@ reg add "HKCU\Software\Classes\ms-officeapp\Shell\Open\Command" /d "rundll32" /t
 :: Disable memory dump from devices
 reg add "HKLM\System\ControlSet001\Control\CrashControl\StorageTelemetry" /v "DeviceDumpEnabled" /t REG_DWORD /d 0 /f
 
-:: Dont reduce	
-fix(onedrive): don't strip if files exist
-	
-Nov 29, 2023
-PFP.ps1 sound volume in calls
+:: Dont reduce sound volume in calls
 reg add "HKCU\SOFTWARE\Microsoft\Multimedia\Audio" /v "UserDuckingPreference" /t REG_DWORD /d 3 /f
 
 :: ewww yucky spell checking
@@ -888,11 +887,72 @@ powercfg /h on
 :: remove defaultuser0 cuz it sucks
 net user defaultuser0 /delete
 
-:: Uninstall Onedrive
-taskkill /f /im OneDrive.exe
-%SystemRoot%\System32\OneDriveSetup.exe /uninstall
-%SystemRoot%\SysWOW64\OneDriveSetup.exe /uninstall
-reg delete "HKEY_CURRENT_USER\Environment" /v "OneDrive" /f
+:: Uninstall Onedrive https://github.com/Atlas-OS/Atlas/blob/main/src/playbook/Executables/ONED.cmd
+
+for /f "usebackq delims=" %%a in (`dir /b /a:d "%SystemDrive%\Users"`) do (
+	if exist "%SystemDrive%\Users\%%a\OneDrive" (
+		dir "%SystemDrive%\Users\%%a\OneDrive" /b | findstr "." > nul 2>&1 && (
+			echo Not stripping OneDrive as OneDrive files exist, exiting...
+			exit 6000
+		)
+	)
+)
+
+taskkill /f /im OneDrive.exe > nul 2>&1
+for %%a in (
+	"%windir%\System32\OneDriveSetup.exe"
+	"%windir%\SysWOW64\OneDriveSetup.exe"
+) do (
+	if exist "%%a" (
+		"%%a" /uninstall > nul 2>&1
+	)
+)
+
+:: If the "Volatile Environment" key exists, that means it is a proper user. Built in accounts/SIDs don't have this key.
+for /f "usebackq tokens=2 delims=\" %%a in (`reg query HKU ^| findstr /r /x /c:"HKEY_USERS\\S-.*" /c:"HKEY_USERS\\AME_UserHive_[^_]*"`) do (
+    reg query "HKU\%%a" | findstr /c:"Volatile Environment" /c:"AME_UserHive_" > nul && (
+        echo Making changes for "%%a"...
+        call :USERREG "%%a"
+    )
+)
+
+rmdir /q /s "%ProgramData%\Microsoft OneDrive" > nul 2>&1
+rmdir /q /s "%LOCALAPPDATA%\Microsoft\OneDrive" > nul 2>&1
+
+for /f "usebackq delims=" %%a in (`dir /b /a:d "%SystemDrive%\Users"`) do (
+	rmdir /q /s "%SystemDrive%\Users\%%a\AppData\Local\Microsoft\OneDrive" > nul 2>&1
+	rmdir /q /s "%SystemDrive%\Users\%%a\OneDrive" > nul 2>&1
+	del /q /f "%SystemDrive%\Users\%%a\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\OneDrive.lnk" > nul 2>&1
+)
+
+for /f "usebackq delims=" %%a in (`reg query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\SyncRootManager" ^| findstr /i /c:"OneDrive"`) do reg delete "%%a" /f > nul 2>&1
+
+for /f "tokens=2 delims=\" %%a in ('schtasks /query /fo list /v ^| findstr /c:"\OneDrive Reporting Task" /c:"\OneDrive Standalone Update Task"') do (
+	schtasks /delete /tn "%%a" /f > nul 2>&1
+)
+
+exit /b
+
+:USERREG
+for /f "usebackq delims=" %%a in (`reg query "HKU\%~1\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\BannerStore" 2^>nul ^| findstr /i /c:"OneDrive" 2^>nul`) do (
+	reg delete "%%a" /f > nul 2>&1
+)
+for /f "usebackq delims=" %%a in (`reg query "HKU\%~1\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\AutoplayHandlers\Handlers" 2^>nul ^| findstr /i /c:"OneDrive" 2^>nul`) do (
+	reg delete "%%a" /f > nul 2>&1
+)
+for /f "usebackq delims=" %%a in (`reg query "HKU\%~1\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths" 2^>nul ^| findstr /i /c:"OneDrive" 2^>nul`) do (
+	reg delete "%%a" /f > nul 2>&1
+)
+for /f "usebackq delims=" %%a in (`reg query "HKU\%~1\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall" 2^>nul ^| findstr /i /c:"OneDrive" 2^>nul`) do (
+	reg delete "%%a" /f > nul 2>&1
+)
+
+reg add "HKU\%~1\SOFTWARE\Classes\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /v "System.IsPinnedToNameSpaceTree" /t REG_DWORD /d "0" /f > nul 2>&1
+reg add "HKU\%~1\SOFTWARE\Classes\WOW6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /v "System.IsPinnedToNameSpaceTree" /t REG_DWORD /d "0" /f > nul 2>&1
+reg delete "HKU\%~1\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Desktop\NameSpace\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /f > nul 2>&1
+
+reg delete "HKU\%~1\Environment" /v "OneDrive" /f > nul 2>&1
+reg delete "HKU\%~1\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "OneDriveSetup" /f > nul 2>&1
 
 setlocal enabledelayedexpansion
 
