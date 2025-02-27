0e2f980a44209f4b6ccd1a2536a29d7820742285
infradragon <infradragon7@gmail.com>
2025-02-27 03:43:26 -0600

    dumb stupid

--- FONDLER.cmd
+++ FONDLER.cmd
@@ -971,17 +971,7 @@ powercfg /h on
 :: remove defaultuser0 cuz it sucks
 net user defaultuser0 /delete
 
-:: Uninstall Onedrive https://github.com/Atlas-OS/Atlas/blob/main/src/playbook/Executables/ONED.cmd
-
-for /f "usebackq delims=" %%a in (`dir /b /a:d "%SystemDrive%\Users"`) do (
-	if exist "%SystemDrive%\Users\%%a\OneDrive" (
-		dir "%SystemDrive%\Users\%%a\OneDrive" /b | findstr "." > nul 2>&1 && (
-			echo Not stripping OneDrive as OneDrive files exist, exiting...
-			exit 6000
-		)
-	)
-)
-
+:: Uninstall Onedrive 
 taskkill /f /im OneDrive.exe > nul 2>&1
 for %%a in (
 	"%windir%\System32\OneDriveSetup.exe"
@@ -990,14 +980,6 @@ for %%a in (
 	if exist "%%a" (
 		"%%a" /uninstall > nul 2>&1
 	)
-)
-
-:: If the "Volatile Environment" key exists, that means it is a proper user. Built in accounts/SIDs don't have this key.
-for /f "usebackq tokens=2 delims=\" %%a in (`reg query HKU ^| findstr /r /x /c:"HKEY_USERS\\S-.*" /c:"HKEY_USERS\\AME_UserHive_[^_]*"`) do (
-    reg query "HKU\%%a" | findstr /c:"Volatile Environment" /c:"AME_UserHive_" > nul && (
-        echo Making changes for "%%a"...
-        call :USERREG "%%a"
-    )
 )
 
 rmdir /q /s "%ProgramData%\Microsoft OneDrive" > nul 2>&1
@@ -1015,28 +997,25 @@ for /f "tokens=2 delims=\" %%a in ('schtasks /query /fo list /v ^| findstr /c:"\
 	schtasks /delete /tn "%%a" /f > nul 2>&1
 )
 
-exit /b
-
-:USERREG
-for /f "usebackq delims=" %%a in (`reg query "HKU\%~1\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\BannerStore" 2^>nul ^| findstr /i /c:"OneDrive" 2^>nul`) do (
+for /f "usebackq delims=" %%a in (`reg query "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\BannerStore" 2^>nul ^| findstr /i /c:"OneDrive" 2^>nul`) do (
 	reg delete "%%a" /f > nul 2>&1
 )
-for /f "usebackq delims=" %%a in (`reg query "HKU\%~1\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\AutoplayHandlers\Handlers" 2^>nul ^| findstr /i /c:"OneDrive" 2^>nul`) do (
+for /f "usebackq delims=" %%a in (`reg query "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\AutoplayHandlers\Handlers" 2^>nul ^| findstr /i /c:"OneDrive" 2^>nul`) do (
 	reg delete "%%a" /f > nul 2>&1
 )
-for /f "usebackq delims=" %%a in (`reg query "HKU\%~1\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths" 2^>nul ^| findstr /i /c:"OneDrive" 2^>nul`) do (
+for /f "usebackq delims=" %%a in (`reg query "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths" 2^>nul ^| findstr /i /c:"OneDrive" 2^>nul`) do (
 	reg delete "%%a" /f > nul 2>&1
 )
-for /f "usebackq delims=" %%a in (`reg query "HKU\%~1\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall" 2^>nul ^| findstr /i /c:"OneDrive" 2^>nul`) do (
+for /f "usebackq delims=" %%a in (`reg query "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall" 2^>nul ^| findstr /i /c:"OneDrive" 2^>nul`) do (
 	reg delete "%%a" /f > nul 2>&1
 )
 
-reg add "HKU\%~1\SOFTWARE\Classes\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /v "System.IsPinnedToNameSpaceTree" /t REG_DWORD /d "0" /f > nul 2>&1
-reg add "HKU\%~1\SOFTWARE\Classes\WOW6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /v "System.IsPinnedToNameSpaceTree" /t REG_DWORD /d "0" /f > nul 2>&1
-reg delete "HKU\%~1\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Desktop\NameSpace\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /f > nul 2>&1
+reg add "HKCU\SOFTWARE\Classes\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /v "System.IsPinnedToNameSpaceTree" /t REG_DWORD /d "0" /f > nul 2>&1
+reg add "HKCU\SOFTWARE\Classes\WOW6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /v "System.IsPinnedToNameSpaceTree" /t REG_DWORD /d "0" /f > nul 2>&1
+reg delete "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Desktop\NameSpace\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /f > nul 2>&1
 
-reg delete "HKU\%~1\Environment" /v "OneDrive" /f > nul 2>&1
-reg delete "HKU\%~1\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "OneDriveSetup" /f > nul 2>&1
+reg delete "HKCU\Environment" /v "OneDrive" /f > nul 2>&1
+reg delete "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "OneDriveSetup" /f > nul 2>&1
 
 setlocal enabledelayedexpansion
 
